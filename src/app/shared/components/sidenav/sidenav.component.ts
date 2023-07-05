0ce8fb411184f51677970eda8749ea9c4b395ec2
Alexandre Guedes <78328104+aledguedes@users.noreply.github.com>
2023-07-05 17:55:23 -0300

    header and logic by close open menu

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataRxjsService } from 'src/app/services/data-rxjs.service';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
+
+  sidenavOpen: boolean = false;
+  showFiller: boolean = false;
+
+  constructor(
+    private rxjs: DataRxjsService,
+  ) {}
+
+  ngOnInit(): void {
+    this.rxjs.toggleSidenav$.subscribe(evt => {
+      this.sidenavOpen = evt;
+    });
+  }
+  
 
 }
