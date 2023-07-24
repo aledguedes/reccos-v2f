b6c10976f79ecf41853fbcdc2eb5ac9a28c0a258
Alexandre Guedes <78328104+aledguedes@users.noreply.github.com>
2023-07-24 18:07:23 -0300

    Componentes

--- src/app/pages/dashboard/components/dash-admin/dash-admin.component.ts
+++ src/app/pages/dashboard/components/dash-admin/dash-admin.component.ts
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class DashAdminComponent {
 
+  displayedColumns: string[] = ['id', 'name', 'address', 'price'];
+  dataSource = ELEMENT_DATA;
 }
+
+const ELEMENT_DATA = [
+  {
+    id: 1,
+    name: 'John Doe',
+    address: 'St. James College',
+    price: '$120'
+  },
+  {
+    id: 2,
+    name: 'John Doe',
+    address: 'St. James College',
+    price: '$120'
+  },
+  {
+    id: 3,
+    name: 'John Doe',
+    address: 'St. James College',
+    price: '$120'
+  },
+  {
+    id: 4,
+    name: 'John Doe',
+    address: 'St. James College',
+    price: '$120'
+  },
+  {
+    id: 5,
+    name: 'John Doe',
+    address: 'St. James College',
+    price: '$120'
+  },
+  {
+    id: 6,
+    name: 'John Doe',
+    address: 'St. James College',
+    price: '$120'
+  }
+]
--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashAdminComponent } from './components/dash-admin/dash-admin.component';
+import { DashUserComponent } from './components/dash-user/dash-user.component';
 
 const routes: Routes = [
   {
     path: '', component: DashboardComponent, children:
-    [
-      { path: '', component: DashAdminComponent}
-    ]
+      [
+        // { path: '', component: DashAdminComponent },
+        { path: '', component: DashUserComponent }
+      ]
   }
 ];
 
