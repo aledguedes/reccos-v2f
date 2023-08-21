7981afe6ecc373fb8012cd1ac904db49bf39e96a
Alexandre Guedes <78328104+aledguedes@users.noreply.github.com>
2023-08-21 10:20:21 -0300

    Developer

---
+++ src/app/utils/dash-admin-menu.ts
@@ -0,0 +1,51 @@
+export const dashAdminMenu = [
+    {
+        'id': 1,
+        'name': 'Usuários',
+        'icon': 'people_outline',
+        'text': 'Cadastro e edição de usuários',
+        'router': '/user'
+    },
+    {
+        'id': 2,
+        'name': 'Federações',
+        'icon': 'emoji_events',
+        'text': 'Cadastro e edição de federações',
+        'router': '/federation'
+    },
+    {
+        'id': 3,
+        'name': 'Ligas',
+        'icon': 'sports_soccer',
+        'text': 'Cadastro e edição de ligas',
+        'router': '/league'
+    },
+    {
+        'id': 4,
+        'name': 'Times',
+        'icon': 'sports_soccer',
+        'text': 'Cadastro e edição de times',
+        'router': '/'
+    },
+    {
+        'id': 5,
+        'name': 'Estádios',
+        'icon': 'sports_soccer',
+        'text': 'Cadastro e edição de estádios',
+        'router': '/'
+    },
+    {
+        'id': 6,
+        'name': 'Atletas',
+        'icon': 'sports_soccer',
+        'text': 'Cadastro e edição de atletas',
+        'router': '/'
+    },
+    {
+        'id': 7,
+        'name': 'Árbitros',
+        'icon': 'sports_soccer',
+        'text': 'Cadastro e edição de arbitros',
+        'router': '/'
+    },
+];
